c8ff72849e940aaeef3ac16fe15d3f17bd52eddd
WahyuSeptiadi <arstuw@gmail.com>
2020-08-15 10:39:51 +0700

    delete doctorlist successful

--- app/src/main/java/com/wahyu/waitinglistapps/Adapter/DoctorListAdapter.java
+++ app/src/main/java/com/wahyu/waitinglistapps/Adapter/DoctorListAdapter.java
@@ -1,19 +1,38 @@
 package com.wahyu.waitinglistapps.Adapter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import com.wahyu.waitinglistapps.Model.DoctorModel;
+import com.wahyu.waitinglistapps.Model.UserModel;
 import com.wahyu.waitinglistapps.R;
+import com.wahyu.waitinglistapps.View.Activity.DoctorDetailsActivity;
+import com.wahyu.waitinglistapps.View.Activity.DoctorListActivity;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,6 +46,9 @@ public class DoctorListAdapter extends RecyclerView.Adapter<DoctorListAdapter.Vi
     private Activity mActivity;
     private ArrayList<DoctorModel> doctorModelArrayList;
 
+    private String userType;
+    private UserModel userModel;
+
     public DoctorListAdapter(Activity mActivity, ArrayList<DoctorModel> doctorModelArrayList) {
         this.mActivity = mActivity;
         this.doctorModelArrayList = doctorModelArrayList;
@@ -36,7 +58,8 @@ public class DoctorListAdapter extends RecyclerView.Adapter<DoctorListAdapter.Vi
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(mActivity).inflate(R.layout.list_item_doctor, parent, false);
-
+        userModel = new UserModel();
+        getTypeUser();
         return new ViewHolder(view);
     }
 
@@ -52,14 +75,36 @@ public class DoctorListAdapter extends RecyclerView.Adapter<DoctorListAdapter.Vi
         holder.tvNameDr.setText(doctorModel.getName());
         holder.tvSpesialis.setText(doctorModel.getSpesialis());
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
+        holder.itemView.setOnClickListener(view -> {
+            Intent toDetails = new Intent(mActivity, DoctorDetailsActivity.class);
+            mActivity.startActivity(toDetails);
         });
 
+        holder.itemView.setOnLongClickListener(view -> {
+            if (userType.equals("admin")) {
+                final Dialog dialog = new Dialog(mActivity);
+                Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.setContentView(R.layout.dialog_edit_delete);
+                dialog.show();
 
+                Button editButton = dialog.findViewById(R.id.btnEdit);
+                Button deleteButton = dialog.findViewById(R.id.btnDelete);
+
+                //apabila tombol edit diklik
+                editButton.setOnClickListener(view1 -> {
+                            dialog.dismiss();
+                        }
+                );
+
+                //apabila tombol delete diklik
+                deleteButton.setOnClickListener(view2 -> {
+                            dialog.dismiss();
+                            showDialogAlertDelete(doctorModel.getId());
+                        }
+                );
+            }
+            return true;
+        });
     }
 
     @Override
@@ -78,5 +123,50 @@ public class DoctorListAdapter extends RecyclerView.Adapter<DoctorListAdapter.Vi
             tvNameDr = itemView.findViewById(R.id.tv_name_doctorlist);
             tvSpesialis = itemView.findViewById(R.id.tv_spesialis_doctorlist);
         }
+    }
+
+    private void getTypeUser() {
+        DatabaseReference rootRoomChats = FirebaseDatabase.getInstance().getReference("Users");
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert firebaseUser != null;
+        String userid = firebaseUser.getUid();
+
+        rootRoomChats.child(userid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userModel = snapshot.getValue(UserModel.class);
+                assert userModel != null;
+
+                userType = userModel.getType();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void showDialogAlertDelete(String key) {
+        DatabaseReference rootDoctors = FirebaseDatabase.getInstance().getReference("Doctors");
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(mActivity);
+        alertDialogBuilder.setTitle("HAPUS DATA DOKTER");
+        alertDialogBuilder
+                .setMessage("Apakah Anda yakin ingin menghapus data user ini ?")
+                .setCancelable(false)
+                .setPositiveButton("Ya, tentu", (dialog, id) -> {
+                    rootDoctors.child(key).removeValue();
+
+                    mActivity.overridePendingTransition(0, 0);
+                    mActivity.startActivity(mActivity.getIntent());
+                    mActivity.finish();
+                    mActivity.overridePendingTransition(0, 0);
+
+                    Toast.makeText(mActivity, "Data berhasil dihapus", Toast.LENGTH_SHORT).show();
+                })
+                .setNegativeButton("Gak jadi", (dialog, id) -> dialog.cancel());
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
